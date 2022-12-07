6fee867e35e58f04783b2617e9b52289514cc225
Apoorv Maurya <90304195+AP0RV@users.noreply.github.com>
2022-12-08 01:41:12 +0530

    Add files via upload

---
+++ BT2008/Common/footer.php
@@ -0,0 +1,43 @@
+<footer>
+  <div class="footer-top">
+    <div class="container">
+        <div class="row">
+          <div class="col-md-3 col-sm-6 col-xs-12 segment-one md-mb-30 sm-mb-30">
+            <h3>Project BT2008</h3>
+            <p>This Project has been made by Apoorv Maurya, Abhay Yadav, Aditya Raj and Suchit Thakur with the help of 
+              our guide Dr. Nitin Mishra
+              
+            </p>
+          </div>
+          <div class="col-md-2 col-sm-6 col-xs-12 segment-two md-mb-30 sm-mb-30">
+            <h2>Quick Links</h2>
+            <ul>
+              <li><a href="">About BT2008</a></li>
+              <li><a href="">Admissions Policy</a></li>
+              <li><a href="">Apply for Admission</a></li>
+              <li><a href="">Download Prospectus</a></li>
+              <li><a href="">Contact Us</a></li>
+            </ul>
+          </div>
+          <div class="col-md-4 col-sm-6 col-xs-12 segment-two md-mb-30 sm-mb-30">
+            <h2>UNDERGRADUATE PROGRAMS</h2>
+            <ul>
+              <li><a href="">B-Tech Civil Engineering</a></li>
+              <li><a href="">B-Tech Electrical Engineering</a></li>
+              <li><a href="">B-Tech Mechanical Engineering</a></li>
+              <li><a href="">B. Architecture</a></li>
+              <li><a href="">B-Tech Computer Science</a></li>
+              <li><a href="">Bachelors of Business Administration</a></li>
+            </ul>
+          </div>  
+          <div class="col-md-3 col-sm-6 col-xs-12 segment-two md-mb-30 sm-mb-30">
+            <h2>CONTACT DETAILS</h2>
+              <p>Address: BT2008 still at development</p>
+              <p>Telephones: +91 945xxxx624</p>
+              <p>E-mail: admissions@bt2008.edu.in</p>
+        </div>
+      </div>
+    </div>
+    </div>
+  <p class="footer-bottom-text">CopyRights © reserved by AAAS Group Members session 2021-25</p>
+</footer>
